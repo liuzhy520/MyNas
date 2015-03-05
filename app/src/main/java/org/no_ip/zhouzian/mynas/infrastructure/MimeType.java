63e9acd71f7b03fb7fea4d45b9c4b4017dfeb9ab
ZIAN ZHOU <zhouzian@hotmail.com>
2015-03-04 21:42:31 -0500

    Added more supported audio formats
    Removed pdf since no application can stream it.

--- app/src/main/java/org/no_ip/zhouzian/mynas/infrastructure/MimeType.java
+++ app/src/main/java/org/no_ip/zhouzian/mynas/infrastructure/MimeType.java
@@ -11,9 +11,11 @@ public class MimeType {
         }
         switch (extension) {
             case "mp3": return "audio/mpeg";
+            case "aac": return "audio/aac";
+            case "ogg": return "audio/ogg";
+            case "flac": return "audio/flac";
             case "mp4": return "video/mp4";
             case "avi": return "video/x-msvideo";
-            case "pdf": return "application/pdf";
             default: return "";
         }
     }
